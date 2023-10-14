6569c72cece8d217109d6b390adfdc79ce9520a6
Krzysztof <Krzysztof@DESKTOP-EJ7O61F>
2023-10-14 11:02:49 +0200

    ChallengeApp2

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "female";
+var age = 33;
+
+if (age < 30 && sex == "female")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (age < 18 && sex != "female")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
